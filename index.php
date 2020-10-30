d4e1bc8c8556482c9c0a1507af31c884aa2c86b5
ananya63070182 <73657454+ananya63070182@users.noreply.github.com>
2020-10-30 15:07:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php
-echo"<h2>IT@KMITL</h2>";
-echo"Hello IT<br>";
-?>
+<html>
+    <head>
+        <title>Lab-ITF-11</title>
+    </head>
+    <body>
+        <h1>Ananya Jinapong</h1>
+        <p>My First Paragraph</p>
+        <img src="cat.jpg">
+        <br>
+        <a href="http:/www.it.kmitl.ac.th">This is a link</a>
+        
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+
+    </body>
+</html>
