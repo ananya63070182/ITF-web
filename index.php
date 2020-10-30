d22a98e25e58f9a844c0453bc1f236b5b197b75d
ananya63070182 <73657454+ananya63070182@users.noreply.github.com>
2020-10-30 15:18:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,30 @@
 <html>
     <head>
+        <meta charset="UTF-8">
         <title>Lab-ITF-11</title>
     </head>
     <body>
         <h1>Ananya Jinapong</h1>
-        <p>My First Paragraph</p>
-        <img src="cat.jpg">
+        <p>Pleng</p>
+        <img src="bingsoo.jpg">
+        <img src="honeytoast.jpg">
         <br>
-        <a href="http:/www.it.kmitl.ac.th">This is a link</a>
+        <a href="https://www.spotify.com/th-th/">This is a link</a>
         
-        <h2>The unordered list</h2>
+        <h2>The desert that I love</h2>
         <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
+            <li>Cake</li>
+            <li>Ice-cream</li>
+            <li>Pancake</li>
         </ul>
+        <br>
 
+        <h3>เรื่องตลกที่จะเล่า</h3>
+        <p>มีมนุษย์ผู้ชายอยู่ 5 ประเภท ที่ผู้หญิงจะต้องปฏิบัติตามคำสั่งด้วยความยินดี 
+            หมอ = ถอดเสื้อผ้าออกสิ 
+            หมอฟัน = อ้ากว้างๆ หน่อยสิ 
+            คนส่งนม = ประตูหน้าหรือประตูหลังครับ 
+            นักตกแต่งภายใน = ถ้าคุณเอาไอ้นี่เข้าไปแล้ว คุณจะชอบมันแน่ๆ 
+            นายธนาคาร = ถ้าคุณถอนออกตอนนี้ คุณจะเสียใจนะครับ</p>
     </body>
 </html>
